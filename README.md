237338a0a67d83225f1499d68bbcef8b4066d871
Dominik Wilusz <wilusz1@o2.pl>
2018-09-11 21:06:42 +0200

    Add sending files

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Serwujemy-pliki
+
+### To install packages run
+```sh
+$ npm install
+```
+
+### To start aplikation run
+```sh
+$ npm start
+```
+
+### Go to localhost:8080/joke to get something about Chuck Norris
--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 var http = require('http');
 var server = http.createServer();
+var fs = require('fs');
 
 server.on('request', function (request, response) {
+    var message;
+
     response.setHeader('Content-Type', 'text/html; charset=utf-8');
 
-    if(request.method === 'GET' && request.url === '/hello') {
-        response.write('<h1>Hello World!</h1>')
-        response.end();
+    if(request.method === 'GET' && request.url === '/joke') {
+        message = getHTML();
     } else {
+        message = getImg();
         response.statusCode = 404;
-        response.write('<h1>404: Zła ścieżka!</h1>');
-        response.end();
     }
+
+    console.log(message);
+    
+    response.write(message);
+    response.end();
 });
 server.listen(8080);
+
+function getHTML() {
+    fs.readFile('./index.html', 'utf-8', function(err, data) {
+        console.log(data);
+        return data;
+    });
+};
+
+function getImg() {
+    fs.stat('./featured_404.jpg', function(err, stats) {
+        if (err) throw err;
+
+        return stats;
+    });
+};
