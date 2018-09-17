7aa1accf0510ef579bef942f4e6d46e194855777
Dominik Wilusz <wilusz1@o2.pl>
2018-09-17 11:32:12 +0200

    Add while to wait for response

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
 var http = require('http');
 var server = http.createServer();
 var fs = require('fs');
+var isMessage = false;
 
 server.on('request', function (request, response) {
+    var message;
+
     response.setHeader('Content-Type', 'text/html; charset=utf-8');
 
     if(request.method === 'GET' && request.url === '/joke') {
-        getHTML(sendResponse);
+        while(!isMessage) {
+            message = getHTML();
+        }
     } else {
-        getImg(sendResponse);
+        while(!isMessage) {
+            message = getImg();
+        }
         response.statusCode = 404;
     }
+    
+    response.write(message.toString());
+    response.end();
+    isMessage = false;
 });
 server.listen(8080);
 
-function sendResponse(message) {
-    response.write(message.toString());
-    response.end();
-}
-
 function getHTML() {
-    fs.readFileSync('./index.html', 'utf-8', function(err, data) {
+    isMessage = true;
+    fs.readFile('./index.html', 'utf-8', function(err, data) {
         if (err) throw err;
 
-        next(data);
+        return data;
     });
 };
 
 function getImg() {
-    fs.statSync('./featured_404.jpg', function(err, stats) {
+    isMessage = true;
+    fs.stat('./featured_404.jpg', function(err, stats) {
         if (err) throw err;
 
-        next(stats);
+        return stats;
     });
 };
