f45b31730cbbf63c84c3ca803bebfce24cb451cb
Dominik Wilusz <wilusz1@o2.pl>
2018-09-18 22:37:11 +0200

    Add function getHtml and getErr

--- index.js
+++ index.js
@@ -6,21 +6,29 @@ server.on('request', function (request, response) {
     response.setHeader('Content-Type', 'text/html; charset=utf-8');
 
     if(request.method === 'GET' && request.url === '/joke') {
-        fs.readFile('./index.html', 'utf-8', function(err, data) {
-            if (err) throw err;
-    
-            response.write(data.toString());
-            response.end();
-        });
+        getHtml(response);
     } else {    
-        fs.readFile("featured_404.jpg", "binary", function(err, file) {
-            if (err) throw err;
-
-            response.writeHead(200, {"Content-Type": "image/png"});
-            response.write(file, "binary");
-            response.end();
-        });
-        response.statusCode = 404;
+        getErr(response);
     }
 });
 server.listen(8080);
+
+function getHtml(response) {
+    fs.readFile('./index.html', 'utf-8', function(err, data) {
+        if (err) throw err;
+
+        response.write(data.toString());
+        response.end();
+    });
+};
+
+function getErr(response) {
+    fs.readFile("featured_404.jpg", "binary", function(err, file) {
+        if (err) throw err;
+
+        response.writeHead(200, {"Content-Type": "image/png"});
+        response.write(file, "binary");
+        response.end();
+    });
+    response.statusCode = 404;
+};
